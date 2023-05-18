9a694a4eda8f012ddf9489e4d200da50e714d833
Vine <chenjinbode@yeah.net>
2023-05-18 15:58:58 +0800

    fisrt commit project

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{title}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data() {
+      return {
+        title: '首页'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
